54fd30ed99c4099334a4a6b8c9477c863add4c84
Sheldon Bentley <sheldonanibentley@gmail.com>
2015-07-29 09:22:09 -0700

    Initial commit of Zelda Dialogue Project. Project demonstrates Zelda-like dialogue. Open Dialogue.Unity scene and hit start to play already set up dialogue.

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class Dialogue : MonoBehaviour
+{
+    private Text _textComponent;
+
+    public string[] DialogueStrings;
+
+    public float SecondsBetweenCharacters = 0.15f;
+    public float CharacterRateMultiplier = 0.5f;
+
+    public KeyCode DialogueInput = KeyCode.Return;
+
+    private bool _isStringBeingRevealed = false;
+    private bool _isDialoguePlaying = false;
+    private bool _isEndOfDialogue = false;
+
+    public GameObject ContinueIcon;
+    public GameObject StopIcon;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _textComponent = GetComponent<Text>();
+	    _textComponent.text = "";
+
+        HideIcons();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	    if (Input.GetKeyDown(KeyCode.Return))
+	    {
+	        if (!_isDialoguePlaying)
+	        {
+                _isDialoguePlaying = true;
+                StartCoroutine(StartDialogue());
+            }
+	        
+	    }
+	}
+
+    private IEnumerator StartDialogue()
+    {
+        int dialogueLength = DialogueStrings.Length;
+        int currentDialogueIndex = 0;
+
+        while (currentDialogueIndex < dialogueLength || !_isStringBeingRevealed)
+        {
+            if (!_isStringBeingRevealed)
+            {
+                _isStringBeingRevealed = true;
+                StartCoroutine(DisplayString(DialogueStrings[currentDialogueIndex++]));
+
+                if (currentDialogueIndex >= dialogueLength)
+                {
+                    _isEndOfDialogue = true;
+                }
+            }
+
+            yield return 0;
+        }
+
+        while (true)
+        {
+            if (Input.GetKeyDown(DialogueInput))
+            {
+                break;
+            }
+
+            yield return 0;
+        }
+
+        HideIcons();
+        _isEndOfDialogue = false;
+        _isDialoguePlaying = false;
+    }
+
+    private IEnumerator DisplayString(string stringToDisplay)
+    {
+        int stringLength = stringToDisplay.Length;
+        int currentCharacterIndex = 0;
+
+        HideIcons();
+
+        _textComponent.text = "";
+
+        while (currentCharacterIndex < stringLength)
+        {
+            _textComponent.text += stringToDisplay[currentCharacterIndex];
+            currentCharacterIndex++;
+
+            if (currentCharacterIndex < stringLength)
+            {
+                if (Input.GetKey(DialogueInput))
+                {
+                    yield return new WaitForSeconds(SecondsBetweenCharacters*CharacterRateMultiplier);
+                }
+                else
+                {
+                    yield return new WaitForSeconds(SecondsBetweenCharacters);
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        ShowIcon();
+
+        while (true)
+        {
+            if (Input.GetKeyDown(DialogueInput))
+            {
+                break;
+            }
+
+            yield return 0;
+        }
+
+        HideIcons();
+
+        _isStringBeingRevealed = false;
+        _textComponent.text = "";
+    }
+
+    private void HideIcons()
+    {
+        ContinueIcon.SetActive(false);
+        StopIcon.SetActive(false);
+    }
+
+    private void ShowIcon()
+    {
+        if (_isEndOfDialogue)
+        {
+            StopIcon.SetActive(true);
+            return;
+        }
+
+        ContinueIcon.SetActive(true);
+    }
+}
